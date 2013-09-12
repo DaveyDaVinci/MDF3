59647026f379cea104b18518a895e29d675e9993
David Magee <daveydavinci@fullsail.edu>
2013-09-12 11:58:51 -0400

    Updated with working sensor data

--- Week2/PhotoOp/gen/com/example/photoop/R.java
+++ Week2/PhotoOp/gen/com/example/photoop/R.java
@@ -24,14 +24,22 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f08000f;
+        public static final int airPressureLabel=0x7f080003;
+        public static final int airPressureValue=0x7f080007;
         public static final int getLocationButton=0x7f080000;
-        public static final int imageViewTest=0x7f080005;
-        public static final int nextPageButton=0x7f080004;
-        public static final int photoButton=0x7f080002;
-        public static final int testText=0x7f080001;
-        public static final int videoButton=0x7f080003;
-        public static final int videoView=0x7f080006;
+        public static final int getSensorsButton=0x7f080001;
+        public static final int humidityLabel=0x7f080005;
+        public static final int humidityValue=0x7f080009;
+        public static final int imageViewTest=0x7f08000d;
+        public static final int lightLabel=0x7f080002;
+        public static final int lightValue=0x7f080006;
+        public static final int nextPageButton=0x7f08000c;
+        public static final int photoButton=0x7f08000a;
+        public static final int temperatureLabel=0x7f080004;
+        public static final int temperatureValue=0x7f080008;
+        public static final int videoButton=0x7f08000b;
+        public static final int videoView=0x7f08000e;
     }
     public static final class layout {
         public static final int activity_gps=0x7f030000;
--- Week2/PhotoOp/src/com/example/photoop/GPSActivity.java
+++ Week2/PhotoOp/src/com/example/photoop/GPSActivity.java
@@ -3,6 +3,10 @@ package com.example.photoop;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -16,11 +20,25 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class GPSActivity extends Activity implements OnClickListener, LocationListener{
+public class GPSActivity extends Activity implements OnClickListener, LocationListener, SensorEventListener{
 
-	LocationManager locationManager;
+	private SensorManager sensorManager;
 	
-	Button getLocationButton;
+	private Sensor lightSensor;
+	private Sensor airPressureSensor;
+	private Sensor temperatureSensor;
+	private Sensor humiditySensor;
+	
+	static LocationManager locationManager;
+	
+	static Button getLocationButton;
+	
+	static Button getSensorsButton;
+	
+	static TextView lightValue;
+	static TextView airPressureValue;
+	static TextView temperatureValue;
+	static TextView humidityValue;
 	
 	
 	
@@ -30,6 +48,19 @@ public class GPSActivity extends Activity implements OnClickListener, LocationLi
 		setContentView(R.layout.activity_gps);
 		
 		getLocationButton = (Button) findViewById(R.id.getLocationButton);
+		
+		getSensorsButton = (Button) findViewById(R.id.getSensorsButton);
+		getSensorsButton.setOnClickListener(this);
+		
+		lightValue = (TextView) findViewById(R.id.lightValue);
+		airPressureValue = (TextView) findViewById(R.id.airPressureValue);
+		temperatureValue = (TextView) findViewById(R.id.temperatureValue);
+		humidityValue = (TextView) findViewById(R.id.humidityValue);
+		
+		
+		
+		
+		
 		
 		
 		
@@ -55,12 +86,58 @@ public class GPSActivity extends Activity implements OnClickListener, LocationLi
 
 	@Override
 	public void onClick(View v) {
-		if (v.equals(this.getLocationButton))
+		if (v.equals(GPSActivity.getLocationButton))
 		{
 			long time = 3*1000;
 			float speed = 0;
 			
 			locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, time, speed, this);
+		}
+		else if (v.equals(GPSActivity.getSensorsButton))
+		{
+			sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+			
+			lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+			airPressureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+			temperatureSensor = sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+			humiditySensor = sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+			
+			if (lightSensor == null)
+			{
+				lightValue.setText("Not available");
+			}
+			else 
+			{
+				sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_UI);
+			}
+			
+			if (airPressureSensor == null)
+			{
+				airPressureValue.setText("Not available");
+				
+			}
+			else 
+			{
+				sensorManager.registerListener(this, airPressureSensor, SensorManager.SENSOR_DELAY_UI);
+			}
+			
+			if (temperatureSensor == null)
+			{
+				temperatureValue.setText("Not available");
+			}
+			else 
+			{
+				sensorManager.registerListener(this, temperatureSensor, SensorManager.SENSOR_DELAY_UI);
+			}
+			
+			if (humiditySensor == null)
+			{
+				humidityValue.setText("Not available");
+			}
+			else
+			{
+				sensorManager.registerListener(this, humiditySensor, SensorManager.SENSOR_DELAY_UI);
+			}
 		}
 		
 	}
@@ -101,6 +178,34 @@ public class GPSActivity extends Activity implements OnClickListener, LocationLi
 	public void onStatusChanged(String provider, int status, Bundle extras) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// TODO Auto-generated method stub
+		
+		if (event.sensor == lightSensor)
+		{
+			lightValue.setText(String.valueOf(event.values[0]));
+		}
+		else if (event.sensor == airPressureSensor)
+		{
+			airPressureValue.setText(String.valueOf(event.values[0]));
+		}
+		else if (event.sensor == temperatureSensor)
+		{
+			temperatureValue.setText(String.valueOf(event.values[0]));
+		}
+		else if (event.sensor == humiditySensor)
+		{
+			humidityValue.setText(String.valueOf(event.values[0]));
+		}
 	}
 
 }
