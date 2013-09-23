1c7630688f0ee926571b6b15312e752cd20a5f62
David Magee <daveydavinci@fullsail.edu>
2013-09-23 15:59:30 -0400

    no message

---
+++ week4/test/WebViewTest/src/com/example/webviewtest/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.webviewtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		WebView testView = (WebView) findViewById(R.id.testWebView);
+		WebSettings webSettings = testView.getSettings();
+		webSettings.setJavaScriptEnabled(true);
+		testView.loadUrl("file:///android_asset/testhtml.html");
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
